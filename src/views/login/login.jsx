ea6416f856b6479d24df4304f6677cbfe91ffacf
Filipe Mota <fmota@blissapplications.com>
2021-01-17 22:09:26 +0000

    testing global var.

--- src/views/login/login.jsx
+++ src/views/login/login.jsx
@@ -22,6 +22,14 @@ const Login = () => {
   const setToken = useSetRecoilState(token);
   const setUser = useSetRecoilState(user);
 
+  console.log(process.env.REACT_APP_API_KEY);
+  console.log(process.env.REACT_APP_AUTH_DOMAIN);
+  console.log(process.env.REACT_APP_DATA_BASE_URL);
+  console.log(process.env.REACT_APP_PROJECT_ID);
+  console.log(process.env.REACT_APP_STORAGE_BUCKET);
+  console.log(process.env.REACT_APP_MESSAGING_SENDER_ID);
+  console.log(process.env.REACT_APP_ID);
+  console.log(process.env.REACT_APP_MEASUREMENT_ID);
   const onSubmit = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase

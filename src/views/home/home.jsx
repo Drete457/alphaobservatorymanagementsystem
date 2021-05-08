30349a47ed0a2d6cb5d275dd6b39a3b469cf55a3
Filipe Mota <drete457@gmail.com>
2021-05-08 16:57:11 +0100

    iframe onLoad have animation

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Button from 'components/button';
+import Loading from 'components/loading';
 
 const Home = () => {
   const [t] = useTranslation();
+  const [isLoading, setIsLoading] = useState(true);
   const linkGoogleCalendar =
     'https://docs.google.com/spreadsheets/d/1BDo7-qqpRMryKrl_1wjKRAkSjGLEVRnp8sYS-Ihbj6I/edit?usp=sharing';
 
@@ -28,10 +31,12 @@ const Home = () => {
             marginWidth="0"
             width="100%"
             height="100%"
+            onLoad={() => setIsLoading(false)}
             src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRd0HowpECfuzV41VbVSKlQJUsWm8QyTkb8iwbYdraEuhq1yq0DPnIQF13rbHhNEjHZeH43SImtORkt/pubhtml?gid=1429204120&amp;single=true&amp;widget=true&amp;headers=false"
             title={t('pages.home.calendar')}
           ></iframe>
         </div>
+        {isLoading && <Loading />}
       </main>
     </>
   );

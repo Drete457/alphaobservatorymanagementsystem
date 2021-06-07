272f2374159eac847625d803a4e9a48f4e5288b0
Filipe Mota <drete457@gmail.com>
2021-06-07 11:45:55 +0100

    download xls file

--- src/helpers/users/exportToExcel.js
+++ src/helpers/users/exportToExcel.js
@@ -1,10 +1,45 @@
 import exportFromJSON from 'export-from-json';
 
-const fileName = 'alphaList';
-const exportType = 'xls';
+const download = (data) => {
+  const fileName = 'alphaList';
+  const exportType = 'xls';
 
-const exportToExcel = (data) => {
   exportFromJSON({ data, fileName, exportType });
+};
+
+const exportToExcel = (data, generic) => {
+  const newData = data.map((user) => {
+    delete user.cardsPosition;
+    delete user.activities;
+    delete user.social;
+    delete user.id;
+
+    const newUser = { ...user };
+    newUser.birthyear = generic.years.find(
+      (value) => value.id === user.birthyear,
+    )?.name;
+    newUser.employment = generic.ocupation.find(
+      (value) => value.id === user.employment,
+    )?.name;
+    newUser.gender = generic.gender.find(
+      (value) => value.id === user.gender,
+    )?.name;
+    newUser.personality = generic.personality.find(
+      (value) => value.id === user.personality,
+    )?.name;
+    newUser.socialInfo.forEach((value) => {
+      const socialName = generic.socialmedia.find(
+        (social) => social.id === value.id,
+      )?.name;
+      newUser[socialName] = value.name;
+    });
+
+    delete newUser.socialInfo;
+
+    return newUser;
+  });
+
+  download(newData);
 };
 
 export default exportToExcel;

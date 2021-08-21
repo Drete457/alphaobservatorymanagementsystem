68102a4d177cd60203a6da37deacb5d2bd90b938
Filipe Mota <drete457@gmail.com>
2021-08-21 13:56:16 +0100

    countries input handler and delete

---
+++ src/helpers/countries/countries-handler.js
@@ -0,0 +1,26 @@
+const countryHandler = (setCountries, countries) => {
+  setCountries([...countries]);
+};
+
+const inputHandler = (event, setCountries, countries, index) => {
+  const key = event.target.getAttribute('name');
+  const value = event.target.value;
+
+  countries[index][key] = value;
+
+  countryHandler(setCountries, countries);
+};
+
+const countryDelete = (index, countries, setCountries) => {
+  const newCountriesArray = [];
+
+  countries?.forEach((country, indexCountry) => {
+    if (indexCountry !== index) {
+      newCountriesArray.push(country);
+    }
+  });
+
+  setCountries(newCountriesArray);
+};
+
+export { countryDelete, inputHandler };

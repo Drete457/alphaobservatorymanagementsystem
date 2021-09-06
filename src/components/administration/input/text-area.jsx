7dd252cd151da1ce9d1c85051ba04a84fbf3e982
Filipe Mota <drete457@gmail.com>
2021-09-07 00:19:41 +0100

    add textarea component generic

---
+++ src/components/administration/input/text-area.jsx
@@ -0,0 +1,19 @@
+import { CTextarea } from '@coreui/react';
+
+const TextAreaField = ({ placeholder, value, onChange, disabled }) => {
+  return (
+    <div>
+      <CTextarea
+        type="type"
+        size="8"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className="input-style"
+        readOnly={disabled}
+      />
+    </div>
+  );
+};
+
+export default TextAreaField;

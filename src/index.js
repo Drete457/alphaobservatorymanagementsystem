db8b873a3bbbcd3d029f7da879792f629476ce99
Filipe Mota <drete457@gmail.com>
2022-04-06 21:06:03 +0100

    update react 17 to 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -48,10 +48,9 @@ if (!image) {
   xhr.send();
 }
 
-ReactDOM.render(
-  <RecoilRoot>{isUpdating ? <Maintenance /> : <App />}</RecoilRoot>,
-  document.getElementById('root'),
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<RecoilRoot>{isUpdating ? <Maintenance /> : <App />}</RecoilRoot>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

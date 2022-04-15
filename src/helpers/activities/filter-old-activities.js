06e85d5dc1523af7be6beb41294067adc5a1bde4
Filipe Mota <drete457@gmail.com>
2022-04-15 14:08:01 +0100

    fix id activities not to show

--- src/helpers/activities/filter-old-activities.js
+++ src/helpers/activities/filter-old-activities.js
@@ -1,7 +1,8 @@
 const filterOlderActivities = (activities) => {
+  console.log(activities);
   const actitiesNotToShow = [
     'b6fddec71394-0fc4f603-af3b986f-026fa186-b0f57c47439ac15c',
-    'a3f33467f9e2-9f5f41d1-ade45059-670a07fe-1081efe10b3934be',
+    '68bfc3947dde-e0dd5abb-2a0ec68f-825f0c3e-620131ab5c2df458',
   ];
   const newArray = activities.filter(
     (activity) => !actitiesNotToShow.includes(activity.id),

cc01f5de505e580d0184c0b86617dbd9a2311b79
Filipe Mota <drete457@gmail.com>
2022-04-29 18:39:40 +0100

    two day after the last activity

--- src/helpers/activities/old-newest-activities.js
+++ src/helpers/activities/old-newest-activities.js
@@ -65,8 +65,9 @@ const calendarToShow = (activities, t) => {
 
   let currentDate = new Date(oldActivity);
   const endDate = new Date(newestActivity);
-  //the endday is always the next dauy after the newest activity
-  endDate.setDate(endDate.getDate() + 1);
+
+  //the endday is always two day after the newest activity
+  endDate.setDate(endDate.getDate() + 2);
 
   while (currentDate <= endDate) {
     if (currentDate.getDay() > 0) {
